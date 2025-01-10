cda64bf34192784e524a624209bc1b723983e36d
El-Bastoi-Si <elbastois@outlook.fr>
2025-01-10 19:01:06 +0100

    Update Exercice9.sh

--- Exercice9.sh
+++ Exercice9.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-curl -o https://github.com/El-Bastoi-Si/Day05/blob/main/Exercice0.sh
-mkdir telechargement 
-cp Exercice0.sh telechargement 
-cd telechargement 
-ls telechargement 
+curl -O https://github.com/El-Bastoi-Si/Day05/blob/main/Exercice0.sh
+mkdir telechargement
+mv Exercice0.sh telechargement
